1efe629e558ddbe6e6d88d553c1b6b8ff87d0f9d
DESKTOP-302-22 <euncoa@naver.com>
2023-04-27 15:30:14 +0900

    huhuhuhu

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ lotto_num = range(1,46)
 
 def buttonClick():
     label=tkinter.Label(window, text=str(random.sample(lotto_num,6)))
-    label=pack()
+    label.pack()
 
     #print(random.sample(lotto_num,6))
 
